23caaf0041ff073f606d4877b55de404483bb2f1
박주연 <wndus51445@gmail.com>
2025-05-22 14:59:56 +0900

    [DELETE] Test Codes

--- src/flink_test_codes/test1.py
+++
@@ -1,8 +0,0 @@
-# my_job.py
-from pyflink.datastream import StreamExecutionEnvironment
-
-env = StreamExecutionEnvironment.get_execution_environment()
-data_stream = env.from_collection([1, 2, 3, 4, 5])
-data_stream.map(lambda x: x * 2).print()
-
-env.execute("Simple Flink Job")
